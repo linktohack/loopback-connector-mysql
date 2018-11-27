625958f6d8dd20f9eeae90e361a5e895bc09a681
Quang-Linh LE <linktohack@gmail.com>
2018-11-27 11:51:40 +0100

    Revert "switch long and lat for geopoint type"
    This reverts commit 3dad2bf4df2b16932af8c271f570f31b35764c55.

--- test/datatypes.test.js
+++ test/datatypes.test.js
@@ -9,7 +9,6 @@ var assert = require('assert');
 var _ = require('lodash');
 
 var db, BlobModel, EnumModel, ANIMAL_ENUM, City, Account;
-
 var mysqlVersion;
 
 describe('MySQL specific datatypes', function() {
@@ -75,7 +74,7 @@ describe('MySQL specific datatypes', function() {
 
     it('create an instance', function(done) {
       Account.create(data, function(err, result) {
-        if (err) return done(err);
+        assert(!err);
         assert(result);
         assert(_.isEqual(data.length, result.length));
         assert(_.isEqual(data[0].amount, result[0].amount));
@@ -86,7 +85,7 @@ describe('MySQL specific datatypes', function() {
 
     it('find an instance', function(done) {
       Account.find(function(err, result) {
-        if (err) return done(err);
+        assert(!err);
         assert(result);
         assert(_.isEqual(data.length, result.length));
         assert(_.isEqual(data[0].amount, result[0].amount));
@@ -99,7 +98,7 @@ describe('MySQL specific datatypes', function() {
 
     it('find an instance by id', function(done) {
       Account.findById(1, function(err, result) {
-        if (err) return done(err);
+        assert(!err);
         assert(result);
         assert(_.isEqual(data[0].amount, result.amount));
         assert(_.isEqual(dateForTransactions[0], result.lastTransaction));
@@ -113,7 +112,7 @@ describe('MySQL specific datatypes', function() {
         amount: 1155.77,
       };
       Account.update({id: 1}, updatedData, function(err, result) {
-        if (err) return done(err);
+        assert(!err);
         assert(result);
         assert(result.count);
         assert.equal(1, result.count);
@@ -141,10 +140,10 @@ describe('MySQL specific datatypes', function() {
 
   it('should create a model instance with Enums', function(done) {
     var em = EnumModel.create({animal: ANIMAL_ENUM.CAT, condition: 'sleepy', mood: 'happy'}, function(err, obj) {
-      if (err) return done(err);
+      assert.ok(!err);
       assert.equal(obj.condition, 'sleepy');
       EnumModel.findOne({where: {animal: ANIMAL_ENUM.CAT}}, function(err, found) {
-        if (err) return done(err);
+        assert.ok(!err);
         assert.equal(found.mood, 'happy');
         assert.equal(found.animal, ANIMAL_ENUM.CAT);
         done();
@@ -169,10 +168,10 @@ describe('MySQL specific datatypes', function() {
     var extras = {c: 3, d: '4'};
     var em = EnumModel.create({animal: ANIMAL_ENUM.DOG, condition: 'sleepy',
       mood: 'happy', note: note, extras: extras}, function(err, obj) {
-      if (err) return done(err);
+      assert.ok(!err);
       assert.equal(obj.condition, 'sleepy');
       EnumModel.findOne({where: {animal: ANIMAL_ENUM.DOG}}, function(err, found) {
-        if (err) return done(err);
+        assert.ok(!err);
         assert.equal(found.mood, 'happy');
         assert.equal(found.animal, ANIMAL_ENUM.DOG);
         assert.deepEqual(found.note, note);
@@ -186,40 +185,12 @@ describe('MySQL specific datatypes', function() {
     var name = 'bob';
     var bob = {name: name, bin: new Buffer.from(str)};
     BlobModel.create(bob, function(err, obj) {
-      if (err) return done(err);
+      assert.ok(!err);
       assert.equal(obj.bin.toString(), str);
       BlobModel.findOne({where: {name: name}}, function(err, found) {
-        if (err) return done(err);
+        assert.ok(!err);
         assert.equal(found.bin.toString(), str);
         done();
-      });
-    });
-  });
-  it('should create a model instance with geopoint type', function(done) {
-    var city1 = {
-      name: 'North York',
-      loc: {
-        lat: 43.761539,
-        lng: -79.411079,
-      },
-    };
-    var xcor, ycor;
-    City.create(city1, function(err, res) {
-      if (err) return done(err);
-      res.loc.should.deepEqual(city1.loc);
-      res.name.should.equal(city1.name);
-      var sqlStmt = 'select ST_X(loc),ST_Y(loc) from City where id=1';
-      db.connector.execute(sqlStmt, function(err, res) {
-        if (err) return done(err);
-        xcor = res[0]['ST_X(loc)'];
-        ycor = res[0]['ST_Y(loc)'];
-        City.find({where: {name: city1.name}}, function(err, found) {
-          if (err) return done(err);
-          found[0].name.should.equal(city1.name);
-          found[0].loc.lng.should.equal(xcor);
-          found[0].loc.lat.should.equal(ycor);
-          done();
-        });
       });
     });
   });
@@ -246,11 +217,6 @@ function setup(done) {
   BlobModel = db.define('BlobModel', {
     bin: {type: Buffer, dataType: 'blob', null: false},
     name: {type: String},
-  });
-
-  City = db.define('City', {
-    name: {type: String},
-    loc: {type: 'GeoPoint'},
   });
   query('SELECT VERSION()', function(err, res) {
     mysqlVersion = res && res[0] && res[0]['VERSION()'];
